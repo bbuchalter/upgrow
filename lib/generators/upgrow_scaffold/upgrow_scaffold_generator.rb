9363608bc4892e66d3337c3840966d4dc7be74cb
Brian Buchalter <bal711@gmail.com>
2021-03-06 06:25:09 -0700

    generator creates records

--- lib/generators/upgrow_scaffold/upgrow_scaffold_generator.rb
+++ lib/generators/upgrow_scaffold/upgrow_scaffold_generator.rb
@@ -1,3 +1,14 @@
 class UpgrowScaffoldGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
+
+  # When a generator is invoked, each public method in the generator is executed sequentially in the order that it is defined.
+  def create_record
+    template('record.rb', "app/records/#{file_name}_record.rb")
+  end
+
+  private
+
+  def parent_class_name_for_records
+    'ApplicationRecord'
+  end
 end
--- test/lib/generators/upgrow_scaffold_generator_test.rb
+++ test/lib/generators/upgrow_scaffold_generator_test.rb
@@ -5,10 +5,29 @@ class UpgrowScaffoldGeneratorTest < Rails::Generators::TestCase
   tests UpgrowScaffoldGenerator
   destination Rails.root.join('tmp/generators')
   setup :prepare_destination
+  arguments %w(article title:string body:text)
 
-  test "generator runs without errors" do
-    assert_nothing_raised do
-      run_generator ["arguments"]
+  test "generator creates only expected files" do
+    files_before_generator = Dir.glob("#{destination_root}/**/*").select { |path| File.file?(path) }.map { |path| path.gsub(destination_root.to_s, '').slice(1..-1) }
+    run_generator
+    files_after_generator = Dir.glob("#{destination_root}/**/*").select { |path| File.file?(path) }.map { |path| path.gsub(destination_root.to_s, '').slice(1..-1) }
+
+    files_created_by_generator = (files_after_generator - files_before_generator).sort
+    expected_files_created_by_generator = %w(app/records/article_record.rb)
+    assert_equal expected_files_created_by_generator, files_created_by_generator
+  end
+
+  test "generator creates app/records/article_record.rb" do
+    run_generator
+
+    assert_file 'app/records/article_record.rb' do |generated_content|
+      expected_content = <<~EXPECTED_CONTENT
+      class ArticleRecord < ApplicationRecord
+        self.table_name = 'articles'
+      end
+      EXPECTED_CONTENT
+
+      assert_equal expected_content, generated_content
     end
   end
 end
