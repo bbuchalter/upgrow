54e5f63b59e9aefb5309c1ff0ce8b88e277a2b1a
Brian Buchalter <bal711@gmail.com>
2021-03-06 03:39:12 -0700

    bin/rails g generator upgrow_scaffold
    rails boilerplate for generators

---
+++ test/lib/generators/upgrow_scaffold_generator_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+require "generators/upgrow_scaffold/upgrow_scaffold_generator"
+
+class UpgrowScaffoldGeneratorTest < Rails::Generators::TestCase
+  tests UpgrowScaffoldGenerator
+  destination Rails.root.join('tmp/generators')
+  setup :prepare_destination
+
+  # test "generator runs without errors" do
+  #   assert_nothing_raised do
+  #     run_generator ["arguments"]
+  #   end
+  # end
+end
